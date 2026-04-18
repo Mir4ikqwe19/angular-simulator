35ae466b6c587f28c8400989cd97d30a4e8a8472
Mir4ikqwe19 <amir.adilov.19@gmail.com>
2026-04-18 12:48:21 +0300

    [Homework №20]: complete reactive froms, input/output tasks

---
+++ src/create-user/create-user.component.ts
@@ -0,0 +1,54 @@
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IUser } from '../interfaces/IUser';
+import { MessageService } from '../services/message.service';
+
+@Component({
+  selector: 'app-create-user',
+  imports: [ReactiveFormsModule],
+  templateUrl: './create-user.component.html',
+  styleUrl: './create-user.component.scss',
+})
+export class CreateUserComponent {
+
+  @Output() createUser: EventEmitter<IUser> = new EventEmitter<IUser>();
+
+  private fb: FormBuilder = inject(FormBuilder);
+  private messageService: MessageService = inject(MessageService);
+
+  createUserForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+    username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(100)]],
+    phone: ['', [Validators.required, Validators.minLength(10),Validators.maxLength(25)]],
+    website: ['', Validators.maxLength(100)],
+    address: this.fb.group({
+      city: ['', [Validators.required, Validators.maxLength(50)]],
+      street: ['', [Validators.required, Validators.maxLength(100)]],
+      suite: ['', [Validators.maxLength(50)]],
+      zipcode: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+      geo: this.fb.group({
+        lat: ['', [Validators.required]],
+        lng: ['', [Validators.required]]
+      }),
+    }),
+    company: this.fb.group({
+      name: ['', [Validators.required, Validators.maxLength(50)]],
+      catchPhrase: ['', [Validators.maxLength(200)]],
+      bs: ['', [Validators.maxLength(100)]]
+    })
+  });
+
+  onSubmit(): void {
+    if (this.createUserForm.valid) {
+      const newUser: IUser = { ...this.createUserForm.getRawValue(), id: Date.now() };
+  
+      this.createUser.emit(newUser);
+      this.messageService.showSucces('Пользователь создан!');
+      this.createUserForm.reset();
+    } else {
+      this.messageService.showError('Не все поля заполнены!');
+    }
+  }
+
+}
